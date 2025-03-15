a06a41b9fd00b9e1b518fe9e927c3a91163c974a
Shahak <Shachakr@gmail.com>
2025-03-15 19:40:53 -0400

    Update network.py

--- src/network.py
+++ src/network.py
@@ -49,7 +49,6 @@ class FeatureExtractionModule(nn.Module):
         for param in resnet.parameters():
             param.requires_grad = False
 
-        
         # Extract first four blocks of ResNet-50
         # ref: https://github.com/pytorch/vision/blob/95131de394543a7c34bd51932bdfce21dae516c1/torchvision/models/resnet.py#L197-L212
         self.block1 = nn.Sequential(*list(resnet.children())[:4])  # conv1 + bn1 + relu + maxpool1
