7823bb047cf92804eb5927bfb03fd7aca8bb20f6
Andrew Luther <90206207+AndrewLuther@users.noreply.github.com>
2025-03-24 11:18:50 -0400

    added adaptation loss

---
+++ src/loss.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+import skimage
+
+from util import Util
+
+# Loss calulations used for test-time adaptation (*** Only used at test time, not during training)
+class Loss():
+    @staticmethod
+    def _min_count_loss(density_map, bboxes):
+        cropped_density_maps = Util.get_examples_from_bboxes(density_map, bboxes)
+        loss = 0
+        # total loss is sum of loss for each cropped density map
+        for crop in cropped_density_maps:
+            # we want the sum of the pixels to be at least 1
+            loss += np.max([0, 1 - torch.sum(crop)])
+        return loss
+            
+    @staticmethod
+    def _perturbation_loss(density_map, bboxes):
+        cropped_density_maps = Util.get_examples_from_bboxes(density_map, bboxes)
+        loss = 0
+        for crop in cropped_density_maps: 
+            dimensions = crop.shape[1:3]
+            std_dev = dimensions[0]/4 # quarter of window size?
+            gaussian_window = skimage.filters.window(('gaussian', std_dev), dimensions)
+            loss += torch.sum((crop - gaussian_window)**2)
+        return loss
+
+    @staticmethod
+    def adaptation_loss(density_map, bboxes, lambda1=10e-9, lambda2=10e-4):
+        return lambda1*Loss.min_count_loss(density_map, bboxes) + lambda2*Loss.perturbation_loss(density_map, bboxes)
--- src/testing.py
+++ src/testing.py
@@ -2,6 +2,7 @@ import torch
 
 from data import Dataset_Creator
 from util import Util
+from loss import Loss
 
 
 # for testing out functionality from other files
@@ -12,4 +13,4 @@ if __name__ == "__main__":
     train_images, train_dmaps, train_bboxes = next(iter(train_loader))
 
     # test with same dmap
-    print(Util.min_count_loss(train_dmaps, train_bboxes[0]))
+    print(Loss.adaptation_loss(train_dmaps, train_bboxes[0]))
